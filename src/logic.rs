22a507bb6e41ab299bf329b8022619a024c2ca8c
Jaques, Sam <sam.jaques@me.com>
2022-12-28 14:14:46 +0000

    feat: initialize BattleSnake

--- src/logic.rs
+++ src/logic.rs
@@ -25,10 +25,10 @@ pub fn info() -> Value {
 
     return json!({
         "apiversion": "1",
-        "author": "", // TODO: Your Battlesnake Username
-        "color": "#888888", // TODO: Choose color
-        "head": "default", // TODO: Choose head
-        "tail": "default", // TODO: Choose tail
+        "author": "DrumSn8ke",
+        "color": "#508c50",
+        "head": "earmuffs",
+        "tail": "nr-booster",
     });
 }
 
